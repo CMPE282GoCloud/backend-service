2f80537d64b8bba92d489d95386280c482a3353a
praveen-sjsu <praveenthakur.sjsu@gmail.com>
2020-03-31 20:53:52 -0700

    Added Deploy Script

--- devops/deploy.sh
+++ devops/deploy.sh
@@ -7,7 +7,5 @@ pip install -r requirements.txt
 zip -r lambda.zip *
 
 # Deploy using AWS CLI
-echo $AWS_ACCESS_KEY_ID
-echo $AWS_SECRET_ACCESS_KEY
-
-aws lambda --region us-east-1 update-function-code --function-name backend-lambda --zip-file fileb://lambda.zip
+aws lambda --region us-east-1 create-function --function-name backend-lambda --runtime python3.8 --zip-file fileb://lambda.zip --role arn:aws:iam::104349172281:role/lambda-execution-role --h
+andler lambda_function.lambda_handler
