9ab1e02ffc670ccd2efc97ea4527662ef65509e8
praveen-sjsu <praveenthakur.sjsu@gmail.com>
2020-03-31 20:55:45 -0700

    Added AWS region in command

--- devops/deploy.sh
+++ devops/deploy.sh
@@ -7,5 +7,4 @@ pip install -r requirements.txt
 zip -r lambda.zip *
 
 # Deploy using AWS CLI
-aws lambda --region us-east-1 create-function --function-name backend-lambda --runtime python3.8 --zip-file fileb://lambda.zip --role arn:aws:iam::104349172281:role/lambda-execution-role --h
-andler lambda_function.lambda_handler
+aws lambda --region us-east-1 create-function --function-name backend-lambda --runtime python3.8 --zip-file fileb://lambda.zip --role arn:aws:iam::104349172281:role/lambda-execution-role --handler lambda_function.lambda_handler
