63e8001f775ceebe2f1a6380d6b3fedf27814e31
praveen-sjsu <praveenthakur.sjsu@gmail.com>
2020-03-31 20:36:55 -0700

    Added AWS region in command

--- devops/deploy.sh
+++ devops/deploy.sh
@@ -7,4 +7,7 @@ pip install -r requirements.txt
 zip -r lambda.zip *
 
 # Deploy using AWS CLI
+echo $AWS_ACCESS_KEY_ID
+echo $AWS_SECRET_ACCESS_KEY
+
 aws lambda --region us-east-1 update-function-code --function-name backend-lambda --zip-file fileb://lambda.zip
